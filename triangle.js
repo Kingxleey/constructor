b7ae90d78e3cc7f29dd2afa71b325beecb67aed2
Okoli kingsley <kingxleeya@gmail.com>
2022-09-05 10:38:29 +0100

    Little changes made

--- triangle.js
+++ triangle.js
@@ -1,17 +1,23 @@
-const shape = {
-    type: 'triangle',
+let shape = {
+    type: null,
     getType: function() { 
-        return this.type
+        return this.type;
      }
   };
   
-  function Triangle(a, b, c) {
+  function Triangle(a, b, c){
     this.a = a;
     this.b = b;
     this.c = c;
+    Triangle.prototype.type = "Triangle";
   }
-  Triangle.prototype = shape;
+Triangle.prototype = Object.create(shape);
+  Triangle.prototype.constructor = Triangle;
+let triangleObj = new Triangle(1,2,3);
+console.log(triangleObj.getType())
+
   Triangle.prototype.getPerimeter = function() {
     return this.a + this.b + this.c;
   };
-  Triangle.prototype.constructor = Triangle;
+console.log(shape.isPrototypeOf(triangleObj));
+console.log(triangleObj.getPerimeter());
